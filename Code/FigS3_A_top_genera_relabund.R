782b8da2df0513685635c383d5a50b0526282feb
barronmr <40303856+barronmr@users.noreply.github.com>
2022-02-11 13:24:38 -0500

    Update FigS3_A_top_genera_relabund.R

--- Code/FigS3_A_top_genera_relabund.R
+++ Code/FigS3_A_top_genera_relabund.R
@@ -1,5 +1,5 @@
 ###############################################################################
-# Relative Abundance of Top 10 Genera DMM Analysis (Figure S3)
+# Relative Abundance of Top 10 Genera DMM Analysis (Figure)
 #01/17/22
 #Madeline R. Barron
 ###############################################################################
@@ -56,8 +56,16 @@ agg_data <- inner_join(agg_data, design)
 rel_abund <- agg_data %>% 
   group_by(group) %>% # Groups by sample col
   mutate(total_count = sum(count)/6, # Calculating total reads per sample and appending to df
-         rel_abund = count/total_count*100) %>%  # Calculating rel abund for each Otu and appending to df
+         rel_abund = count/total_count) %>%  # Calculating rel abund for each Otu and appending to df
   ungroup()
+
+smallest_non_zero <- rel_abund %>%
+  filter(rel_abund > 0) %>%
+  slice_min(rel_abund) %>%
+  pull(rel_abund) %>% .[1]
+
+rel_abund <- rel_abund %>%
+  mutate(rel_abund_c = rel_abund + smallest_non_zero / 10)
 
 
 agg_otu_rel_abund <- rel_abund %>% 
@@ -68,15 +76,15 @@ agg_otu_rel_abund <- rel_abund %>%
            exp_id_number,
            treatment,
            classification) %>% # Aggregates classifications based on treatment
-  summarize(agg_rel_abund = sum(rel_abund)) %>% # Calculating aggregated rel abund
+  summarize(agg_rel_abund_c = sum(rel_abund_c)) %>% # Calculating aggregated rel abund
   ungroup()
 
 #Finding top tax classifications for a given tax level
 top_otu_tax <- agg_otu_rel_abund %>% 
   group_by(classification) %>%
-  summarize(mean_rel_abund = mean(agg_rel_abund)) %>% # Calculating mean aggregated relative abundance across all samples
-  top_n(n = 10, wt = mean_rel_abund) %>% 
-  filter(mean_rel_abund > 0) %>%  # Only choosing Otus with non-zero mean relative abundances
+  summarize(mean_rel_abund_c = mean(agg_rel_abund_c)) %>% # Calculating mean aggregated relative abundance across all samples
+  top_n(n = 10, wt = mean_rel_abund_c) %>% 
+  filter(mean_rel_abund_c > 0) %>%  # Only choosing Otus with non-zero mean relative abundances
   pull(classification) #Creating vector of Otu tax classifications
 
 # Reassigning any tax classification as "Other" if not in the top_otu_tax list
@@ -90,8 +98,8 @@ top_otu_data <- agg_otu_rel_abund %>%
            exp_id_number,
            treatment,
            classification) %>% # Need to collapse all of the newly labelled "Other" tax into single entries
-  summarize(agg_rel_abund = sum(agg_rel_abund)) %>% 
-  mutate(log_mean_rel_abund = log10(agg_rel_abund + 1)) %>%# Adds up all entries for "Other" classification
+  summarize(agg_rel_abund_c = sum(agg_rel_abund_c)) %>% 
+  mutate(mean_rel_abund_c = log10(agg_rel_abund_c + 1)) %>%# Adds up all entries for "Other" classification
  filter(classification != "Other") %>% 
    ungroup()
 
@@ -100,19 +108,20 @@ top_otu_data <- agg_otu_rel_abund %>%
 ###############################################################################
 
 abund<- top_otu_data %>% 
-  ggplot(aes(x=log_mean_rel_abund,group = partition,
+  ggplot(aes(x=mean_rel_abund_c,
                               y= classification,color = partition)) +
-  geom_jitter(position=position_jitterdodge(dodge.width = 0.7,jitter.width = 0.08), size=2) +
-  labs(x = "Log10 (Relative Abundance +1)",
+  geom_boxplot(lwd =0.75) +
+  geom_vline(xintercept = smallest_non_zero, linetype = 'dashed', color = "gray") +
+  labs(x = expression("Relative Abundance ("*Log[10]*")"),
        y = "OTU",
        color = "Community type") +
   scale_color_brewer(palette = "Set1")+
-  scale_x_continuous(limits = c(0, 3)) + 
-  stat_summary(aes(group=partition),fun=mean,fun.min=mean,fun.max=mean,geom="crossbar",
-               width=0.5,colour="black",position=position_dodge(width=0.7)) +
+  scale_x_log10() + 
   theme_classic() +
   theme(axis.text.y = element_text(size = 9, colour = "black"),
-        axis.text.x = element_text(size = 11, colour = "black", angle = 90),
+        axis.text.x = element_text(size = 11, colour = "black", angle = 90, hjust=1),
+        axis.title.x = element_blank(),
+        legend.title = element_text(),
         text = element_text(colour = "black", size = 11))
 
 abund + coord_flip()
@@ -129,117 +138,64 @@ abund + coord_flip()
 akker <- filter(top_otu_data, classification == "Akkermansia")
 
 
-kruskal.test(log_mean_rel_abund ~ partition, data = akker)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = akker)
+kruskal.test(mean_rel_abund_c ~ partition, data = akker)
+dunn_test(mean_rel_abund_c~ partition, p.adjust.method = "bonferroni", data = akker)
 
-.#y.                group1      group2    n1    n2 statistic       p   p.adj p.adj.signif
-#* <chr>              <chr>       <chr>  <int> <int>     <dbl>   <dbl>   <dbl> <chr>       
- # 1 log_mean_rel_abund Partition_1 Parti…    20    18     -3.58 3.46e-4 0.00104 **          
- # 2 log_mean_rel_abund Partition_1 Parti…    20     4     -3.18 1.45e-3 0.00435 **          
- # 3 log_mean_rel_abund Partition_2 Parti…    18     4     -1.05 2.92e-1 0.877   ns    
 
 #Alistipes
 
+
 alis <- filter(top_otu_data, classification == "Alistipes")
 
-kruskal.test(log_mean_rel_abund ~ partition, data = alis)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = alis)
 
-#  .y.                group1      group2    n1    n2 statistic       p   p.adj p.adj.signif
-#* <chr>              <chr>       <chr>  <int> <int>     <dbl>   <dbl>   <dbl> <chr>       
-#  1 log_mean_rel_abund Partition_1 Parti…    20    18      4.53 5.92e-6 1.77e-5 ****        
-#  2 log_mean_rel_abund Partition_1 Parti…    20     4     -1.85 6.39e-2 1.92e-1 ns          
-#3 log_mean_rel_abund Partition_2 Parti…    18     4     -4.50 6.85e-6 2.05e-5 **** 
+kruskal.test(mean_rel_abund_c ~ partition, data = alis)
+dunn_test(mean_rel_abund_c ~ partition, p.adjust.method = "bonferroni", data = alis)
 
 #Bacteroides
 
 bacteroides <- filter(top_otu_data, classification == "Bacteroides")
-kruskal.test(log_mean_rel_abund ~ partition, data = bacteroides)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = bacteroides)
-
-#Not significant 
+kruskal.test(mean_rel_abund_c ~ partition, data = bacteroides)
+dunn_test(mean_rel_abund_c ~ partition, p.adjust.method = "bonferroni", data = bacteroides)
 
 #Bifidobacterium
 
 bifido <- filter(top_otu_data, classification == "Bifidobacterium")
-kruskal.test(log_mean_rel_abund ~ partition, data = bifido)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = bifido)
-
-#.y.                group1      group2         n1    n2 statistic       p  p.adj p.adj.signif
-#* <chr>              <chr>       <chr>       <int> <int>     <dbl>   <dbl>  <dbl> <chr>       
-#  1 log_mean_rel_abund Partition_1 Partition_2    20    18   -2.60   0.00945 0.0283 *           
- # 2 log_mean_rel_abund Partition_1 Partition_3    20     4    0.0957 0.924   1      ns          
-#3 log_mean_rel_abund Partition_2 Partition_3    18     4    1.62   0.105   0.316  ns  
+kruskal.test(mean_rel_abund_c ~ partition, data = bifido)
+dunn_test(mean_rel_abund_c ~ partition, p.adjust.method = "bonferroni", data = bifido)
 
 #Erysipelotrichaceae
 
 erysip <- filter(top_otu_data, classification == "Erysipelotrichaceae_unclassified")
-kruskal.test(log_mean_rel_abund ~ partition, data = erysip)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = erysip)
-
-# A tibble: 3 × 9
-#.y.                group1      group2         n1    n2 statistic        p    p.adj p.adj.signif
-#* <chr>              <chr>       <chr>       <int> <int>     <dbl>    <dbl>    <dbl> <chr>       
-  #1 log_mean_rel_abund Partition_1 Partition_2    20    18    -3.67  0.000239 0.000716 ***         
-  #2 log_mean_rel_abund Partition_1 Partition_3    20     4    -3.01  0.00265  0.00794  **          
-  #3 log_mean_rel_abund Partition_2 Partition_3    18     4    -0.819 0.413    1        ns  
+kruskal.test(mean_rel_abund_c ~ partition, data = erysip)
+dunn_test(mean_rel_abund_c ~ partition, p.adjust.method = "bonferroni", data = erysip)
 
 #Eneterobaceriaceae
 
 entero <- filter(top_otu_data, classification == "Enterobacteriaceae_unclassified")
-kruskal.test(log_mean_rel_abund ~ partition, data = entero)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = entero)
+kruskal.test(mean_rel_abund_c ~ partition, data = entero)
+dunn_test(mean_rel_abund_c ~ partition, p.adjust.method = "bonferroni", data = entero)
 
-#* <chr>              <chr>       <chr>       <int> <int>     <dbl>      <dbl>      <dbl> <chr>       
- # 1 log_mean_rel_abund Partition_1 Partition_2    20    18     -4.71 0.00000247 0.00000741 ****        
- # 2 log_mean_rel_abund Partition_1 Partition_3    20     4      1.56 0.120      0.360      ns          
-#3 log_mean_rel_abund Partition_2 Partition_3    18     4      4.31 0.0000163  0.0000490  ****        
 
 #Lachnospiraceae
 
 lachno <- filter(top_otu_data, classification == "Lachnospiraceae_unclassified")
-kruskal.test(log_mean_rel_abund ~ partition, data = lachno)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = lachno)
-
-#.y.                group1      group2         n1    n2 statistic         p     p.adj p.adj.signif
-#* <chr>              <chr>       <chr>       <int> <int>     <dbl>     <dbl>     <dbl> <chr>       
- # 1 log_mean_rel_abund Partition_1 Partition_2    20    18      3.96 0.0000758 0.000228  ***         
- # 2 log_mean_rel_abund Partition_1 Partition_3    20     4     -1.89 0.0588    0.176     ns          
-#3 log_mean_rel_abund Partition_2 Partition_3    18     4     -4.20 0.0000269 0.0000806 ****   
+kruskal.test(mean_rel_abund_c ~ partition, data = lachno)
+dunn_test(mean_rel_abund_c ~ partition, p.adjust.method = "bonferroni", data = lachno)
 
 #Lactobacillus 
 
 lacto <- filter(top_otu_data, classification == "Lactobacillus")
-kruskal.test(log_mean_rel_abund ~ partition, data = lacto)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = lacto)
-
-#.y.                group1      group2         n1    n2 statistic       p   p.adj p.adj.signif
-#* <chr>              <chr>       <chr>       <int> <int>     <dbl>   <dbl>   <dbl> <chr>       
- # 1 log_mean_rel_abund Partition_1 Partition_2    20    18     -2.63 0.00846 0.0254  *           
- # 2 log_mean_rel_abund Partition_1 Partition_3    20     4      1.48 0.139   0.416   ns          
-#3 log_mean_rel_abund Partition_2 Partition_3    18     4      3.01 0.00257 0.00771 ** 
+kruskal.test(mean_rel_abund_c ~ partition, data = lacto)
 
 #Porphyromonodaceae
 
 porphy <- filter(top_otu_data, classification == "Porphyromonadaceae_unclassified")
-kruskal.test(log_mean_rel_abund ~ partition, data = porphy)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = porphy)
-
-# A tibble: 3 × 9
-#.y.                group1      group2         n1    n2 statistic        p    p.adj p.adj.signif
-#* <chr>              <chr>       <chr>       <int> <int>     <dbl>    <dbl>    <dbl> <chr>       
-#  1 log_mean_rel_abund Partition_1 Partition_2    20    18      1.85 0.0638   0.191    ns          
-#2 log_mean_rel_abund Partition_1 Partition_3    20     4     -2.60 0.00920  0.0276   *           
- # 3 log_mean_rel_abund Partition_2 Partition_3    18     4     -3.67 0.000242 0.000727 *** 
+kruskal.test(mean_rel_abund_c ~ partition, data = porphy)
+dunn_test(mean_rel_abund_c ~ partition, p.adjust.method = "bonferroni", data = porphy)
 
 #Ruminococcacae 
 
 rumino <- filter(top_otu_data, classification == "Ruminococcaceae_unclassified")
-kruskal.test(log_mean_rel_abund ~ partition, data = rumino)
-dunn_test(log_mean_rel_abund ~ partition, p.adjust.method = "bonferroni", data = rumino)
+kruskal.test(mean_rel_abund_c ~ partition, data = rumino)
+dunn_test(mean_rel_abund_c ~ partition, p.adjust.method = "bonferroni", data = rumino)
 
-# A tibble: 3 × 9
-.#y.                group1      group2         n1    n2 statistic        p    p.adj p.adj.signif*# <chr>              <chr>       <chr>       <int> <int>     <dbl>    <dbl>    <dbl> <chr>       
- # 1 log_mean_rel_abund Partition_1 Partition_2    20    18      3.73 0.000188 0.000565 ***         
- # 2 log_mean_rel_abund Partition_1 Partition_3    20     4     -1.29 0.195    0.586    ns          
-#3 log_mean_rel_abund Partition_2 Partition_3    18     4     -3.48 0.000505 0.00152  **  
